ecf3c10b4c25ee8a83138997b348b985d211a804
Arjun Medapati <arjunmedapati@Arjuns-MacBook-Air.local>
2025-05-15 12:58:58 +0530

    Adding getDep,updateDep,deleteDep end points

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -20,8 +20,9 @@ exports.AddDepartment=async(req,res)=>{
 
         res.status(200).json({
             success:true,
-            message:"Course create successfully",
-            data:newDept
+            message:"Department created successfully",
+            // department:department,
+            newDept
         })
 
     }
@@ -39,3 +40,89 @@ exports.AddDepartment=async(req,res)=>{
 
 }
 
+exports.addDepartment=async (req,res)=>{
+    try{
+        const departments=await model.find();
+        res.status(200).json({
+            success:true,
+            message:'department find successfully',
+            departments
+        })
+
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:'getting ddepatment server error'
+        })
+
+
+    }
+}
+
+
+exports.getDepartment=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const department=await model.findById({_id:id})
+        res.status(200).json({
+            success:true,
+            department
+        })
+
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            error:'Error while editing the department'
+
+        })
+
+    }
+
+}
+
+exports.updateDepartment=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const {dept_name,description}=req.body;
+        const updateDepart=await model.findByIdAndUpdate({_id:id},{
+            dept_name,description
+        })
+        res.status(200).json({
+            success:true,
+            updateDepart
+        })
+
+
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            error:'Error while editing the department'
+
+        })
+
+
+    }
+}
+
+exports.deleteDepartment=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const deleteDep=await model.findByIdAndDelete({_id:id})
+        return res.status(200).json({
+            success:true,
+            message:"deleted succesfully",
+            deleteDep
+        })
+
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:"Server error while deleteing the department"
+        })
+
+    }
+}
--- routes/Department.js
+++ routes/Department.js
@@ -4,9 +4,15 @@ const router=express.Router();
 
 
 const {verifyUser}=require("../middleware/authMiddleware")
-const {AddDepartment}=require("../controllers/departmentController")
+const {AddDepartment,addDepartment,getDepartment,updateDepartment, deleteDepartment}=require("../controllers/departmentController")
 
 
 router.post("/add",verifyUser,AddDepartment);
+router.get("/",verifyUser,addDepartment);
+router.get('/:id',verifyUser,getDepartment)
+router.put('/:id',verifyUser,updateDepartment)
+router.delete('/:id',verifyUser,deleteDepartment)
+
+
 
 module.exports=router;
